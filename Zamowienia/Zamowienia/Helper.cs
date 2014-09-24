464ca436f02e39d2937d0d44ef0d7e5d8d24f5e2
RobertD <robertd@j-sas.com>
2014-09-25 00:25:43 +0200

    orders1

---
+++ Zamowienia/Zamowienia/Helper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace Zamowienia
+{
+    public class Helper
+    {
+        private static string connectionString = string.Empty;
+
+        public static void SetConnect()
+        {//pobranie danych z pliku konfiguracyjnego txt
+            string adresServ = "", haslo = "";
+            var file = System.IO.File.OpenText("set.txt");
+            adresServ = file.ReadLine();
+            haslo = file.ReadLine();
+            //connectionString = @"Server=" + adresServ + ";Database=Produkcja_1; User ID=sa;Password=" + haslo + ";";
+            connectionString = @"Server=" + Szyfrowanie.DeSzyfrowanie(adresServ) + ";Database=Produkcja_1; User ID=sa;Password=" + Szyfrowanie.DeSzyfrowanie(haslo) + ";";
+            file.Close();
+        }
+
+        public static SqlConnection GetDBConnection()
+        {
+            if(string.IsNullOrEmpty(connectionString))
+                SetConnect();
+            SqlConnection conn = null;
+            try
+            {
+                conn = new SqlConnection(connectionString);
+                if (conn == null)
+                {
+                    throw new ArgumentNullException();
+                }
+                conn.Open();
+            }
+            catch (Exception ex)
+            {
+                //     Error<Helper>.RegError(ex);
+            }
+            return conn;
+        }
+
+
+
+    }
+}
---
+++ Zamowienia/Zamowienia/pracownik.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zamowienia
+{
+    public class pracownik
+    {
+        private int idPracownik;
+        private string pracownik1;
+        private int admin;
+        private string zmiana;
+
+        public pracownik(int _idPracownik, string _pracownik, int _admin, string _zmiana)
+        {
+            idPracownik = _idPracownik;
+            pracownik1 = _pracownik;
+            admin = _admin;
+            zmiana = _zmiana;
+        }
+
+        public int get_idPracownik()
+        { return idPracownik; }
+
+        public string get_pracownik()
+        { return pracownik1; }
+
+        public int get_user_status()
+        { return admin; }
+
+        public string get_zmiana()
+        { return zmiana; }
+
+        public void set_pracownik_logout()
+        {
+            idPracownik = -1;
+            pracownik1 = "-1";
+            admin = -1;
+            zmiana = "-1";
+        }
+    }
+}
